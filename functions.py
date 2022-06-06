6febbfb921b513ab9e71e7fe562e51d82452d190
Erfan Kheyrollahi <ekm507@gmail.com>
2022-06-06 17:33:23 +0430

    add process text function template

--- functions.py
+++ functions.py
@@ -1,5 +1,26 @@
+import requests
 
 # functions for messages are in this file
 
-def process_text_message(text,chat_id, message_id):
-    pass
+# get a text message and do whatever is needed
+def process_text_message(text:str,chat_id, message_id):
+
+    text_to_send = find_and_replace_links(text)
+
+    # make a message to send to telegram
+    message = {
+        # chat id should be id of the one who had requested
+        "chat_id":chat_id,
+        # text is command output in monospace format
+        "text":'```\n' + text_to_send + '\n```',
+        # set parse mode to markdown so that text can be in monospace
+        "parse_mode":"HTML",
+        "reply_to_message_id":message_id,
+    }
+
+    requests.post(f'https://api.telegram.org/bot{bot_token}/sendMessage', data=message)
+
+
+# this function will find links to services in a message and return alternative links.
+def find_and_replace_links(text:str):
+    return "test"
